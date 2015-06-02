70db9b5aa2937a3ecdc36ab5e22d1f9ecda64899
kevinwu1 <kevinwu1997@gmail.com>
2015-06-02 11:11:39 -0700

    Smooth rendering

--- src/flyingStuff/BallTest1.java
+++ src/flyingStuff/BallTest1.java
@@ -3,16 +3,18 @@ package flyingStuff;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 class BallTest1 extends Canvas implements Runnable {
 	private Ball ball;
 
+	private BufferedImage back;
+
 	public BallTest1() {
 		setBackground(Color.WHITE);
 		setVisible(true);
-
-		ball = new Ball.BallBuilder().X(10).Y(10).R(10).XV(3).YV(3)
-				.C(Color.RED).build();
+		ball = new Ball.BallBuilder().R(10).XV(30).YV(90).C(Color.RED).build();
 		new Thread(this).start();
 	}
 
@@ -23,16 +25,21 @@ class BallTest1 extends Canvas implements Runnable {
 
 	@Override
 	public void paint(Graphics window) {
+		Graphics2D twoDGraph = (Graphics2D) window;
+		if (back == null)
+			back = (BufferedImage) createImage(getWidth(), getHeight());
+		Graphics graphToBack = back.createGraphics();
 
-		ball.moveAndDraw(window);
+		ball.moveAndDraw(graphToBack);
 
-		if (ball.getLeft() < 0 || ball.getRight() > Runner.WIDTH) {
+		if (ball.getLeft() < 0 || ball.getRight() > window.getClipBounds().getWidth()) {
 			ball.setXV(-ball.getXV());
 		}
 
-		if (ball.getTop() < 0 || ball.getBot() > Runner.HEIGHT) {
+		if (ball.getTop() < 0 || ball.getBot() > window.getClipBounds().getHeight()) {
 			ball.setYV(-ball.getYV());
 		}
+		twoDGraph.drawImage(back, null, 0, 0);
 	}
 
 	@Override
@@ -42,7 +49,8 @@ class BallTest1 extends Canvas implements Runnable {
 				Thread.sleep(10);
 				repaint();
 			}
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 		}
 	}
 }
