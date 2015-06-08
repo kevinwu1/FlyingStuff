cb26fcfe2f84f52020557fddf89400b81cd924cb
kevinwu1 <kevinwu1997@gmail.com>
2015-06-08 09:39:03 -0700

    Revert "Revert "Addball cd, mouse listeners, title screen""
    This reverts commit c81fcd3470b406313440da06543ff005211af9c1.

--- src/flyingStuff/BallTest1.java
+++ src/flyingStuff/BallTest1.java
@@ -6,13 +6,21 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 
-class BallTest1 extends Canvas implements Runnable, KeyListener {
+class BallTest1 extends Canvas implements Runnable, KeyListener, MouseListener, MouseMotionListener {
 	BallList balls = new BallList();
 	public static int WIDTH;
 	public static int HEIGHT;
 	private boolean[] keys;
+	private boolean pressed = false;
+	private static final int BALL_CD = 30;
+	private int ballCD = 0;
+	@SuppressWarnings("unused")
+	private int x, y;
 
 	private BufferedImage back;
 
@@ -20,11 +28,12 @@ class BallTest1 extends Canvas implements Runnable, KeyListener {
 		setBackground(Color.WHITE);
 		setVisible(true);
 		balls = new BallList();
-		for (int i = 0; i < 50; i++) {
+		for (int i = 0; i < 1; i++) {
 			balls.add();
 		}
 		keys = new boolean[2];
 		this.addKeyListener(this);
+		this.addMouseListener(this);
 		// System.out.println(balls.getLength());
 		new Thread(this).start();
 	}
@@ -36,22 +45,27 @@ class BallTest1 extends Canvas implements Runnable, KeyListener {
 
 	@Override
 	public void paint(Graphics window) {
-
 		Graphics2D twoDGraph = (Graphics2D) window;
 		if (back == null)
 			back = (BufferedImage) createImage(getWidth(), getHeight());
 		Graphics graphToBack = back.createGraphics();
 		balls.drawAll(graphToBack);
-		if (keys[0] == true) {
+		if (keys[0]) {
+			if ((ballCD = (ballCD + 1) % BALL_CD) == 1)
+				balls.add();
+		}
+		else if (ballCD != 0) {
+			ballCD = (ballCD + 1) % BALL_CD;
+		}
+		if (pressed) {
 
-			balls.add();
 		}
 		twoDGraph.drawImage(back, null, 0, 0);
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+		if (e.getKeyCode() == Runner.addBallKey) {
 
 			keys[0] = true;
 		}
@@ -60,7 +74,7 @@ class BallTest1 extends Canvas implements Runnable, KeyListener {
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+		if (e.getKeyCode() == Runner.addBallKey) {
 			keys[0] = false;
 		}
 
@@ -78,7 +92,45 @@ class BallTest1 extends Canvas implements Runnable, KeyListener {
 				Thread.sleep(10);
 				repaint();
 			}
-		} catch (Exception e) {
 		}
+		catch (Exception e) {
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		System.out.println(e.getX() + ", " + e.getY());
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		pressed = true;
+		x = e.getX();
+		y = e.getY();
+		System.out.println("down: " + e.getX() + ", " + e.getY());
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		System.out.println("up");
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		x = e.getX();
+		y = e.getY();
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
 	}
 }
--- src/flyingStuff/Runner.java
+++ src/flyingStuff/Runner.java
@@ -1,15 +1,22 @@
 package flyingStuff;
 
-import java.awt.Component;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Runner extends JFrame {
 	static Runner run;
-	static final int WIDTH = 800;
-	static final int HEIGHT = 800;
+	static final int WIDTH = 1000;
+	static final int HEIGHT = 1000;
 	static int INNER_WIDTH;
 	static int INNER_HEIGHT;
+	static final int addBallKey = KeyEvent.VK_SPACE;
 
 	public Runner() {
 		super("Bounce");
@@ -23,9 +30,42 @@ public class Runner extends JFrame {
 		run = new Runner();
 		INNER_WIDTH = run.getContentPane().getWidth();
 		INNER_HEIGHT = run.getContentPane().getHeight();
-		BallTest1 padTest = new BallTest1();
-		((Component) padTest).setFocusable(true);
-		run.getContentPane().add(padTest);
+
+		final JPanel titleScreen = new JPanel();
+		titleScreen.setLayout(null);
+
+		JButton startBut = new JButton();
+		startBut.setBackground(Color.GREEN);
+		startBut.setText("Start");
+		startBut.setSize(200, 50);
+		startBut.setLocation((Runner.INNER_WIDTH - startBut.getWidth()) / 2, Runner.INNER_HEIGHT - 100);
+		titleScreen.add(startBut);
+
+		JLabel credits = new JLabel();
+		credits.setText("Made by Kevin Wu and Nathan Mar");
+		credits.setSize(200, 50);
+		credits.setLocation((INNER_WIDTH - 200) / 2, INNER_HEIGHT - credits.getHeight());
+		titleScreen.add(credits);
+
+		JLabel instr = new JLabel("Press space to spawn a ball. Click to make balls converge.");
+		instr.setSize(350, 200);
+		instr.setLocation((INNER_WIDTH - instr.getWidth()) / 2, INNER_HEIGHT - 600);
+		titleScreen.add(instr);
+
+		run.add(titleScreen);
+
+		startBut.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				titleScreen.setVisible(false);
+				run.remove(titleScreen);
+				run.add(new BallTest1());
+			}
+		});
+		// TitleScreen title = new TitleScreen();
+		// ((Component) title).setFocusable(true);
+		// run.getContentPane().add(title);
+
 		// TODO
 		// Mouse click converge.
 		// keyboard buttons add ball,
